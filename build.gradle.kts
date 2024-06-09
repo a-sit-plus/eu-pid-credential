d8ee5ccd2f5902bc4d147d909ae59754e8270843
Bernd Prünster <bernd.pruenster@a-sit.at>
2024-06-09 20:19:27 +0200

    2.0.2: update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,16 @@
 plugins {
-    id("at.asitplus.gradle.conventions") version "1.9.23+20240501"
+    id("at.asitplus.gradle.conventions") version "2.0.0+20240609"
 }
 
 val artifactVersion: String by extra
 group = "at.asitplus.wallet"
 version = artifactVersion
+
+allprojects {
+    repositories {
+        maven {
+            url = uri("https://oss.sonatype.org/content/repositories/snapshots")
+            name = "bigNum"
+        }
+    }
+}
